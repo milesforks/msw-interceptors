030ae11d03000b602fd30ee28a01ee89f18d2ebf
Miles Richardson <miles@splitgraph.com>
2022-08-27 01:46:37 +0100

    chore(wip): find the right patch point

--- test/jest.browser.setup.ts
+++ test/jest.browser.setup.ts
@@ -19,6 +19,26 @@ beforeAll(async () => {
     serverOptions: {
       webpackConfig,
     },
+  }).then((browser) => {
+    // note: we know server connection is up because createBrowser waited `until`
+    // it was up, meaning PreviewServer.listen() set `this.connectionInfo`
+    const conn = server!.connectionInfo!
+
+    // bug: `PreviewServer.listen()` serializes IPv6 hosts to invalid URL (missing square brackets)
+    // see: node_modules/page-with/lib/server/PreviewServer.js / PreviewServer.listen
+
+    // fix: re-serialize any URL containing an IPv6 host without surrounding brackets
+    if (conn.host.includes(':') && !conn.url.includes(`[${conn.host}]`)) {
+      // note: scheme is hardcoded to `http` to match `PreviewServer.listen()`
+      server!.connectionInfo!.url = `http://[${conn.host}]:${conn.port}`
+      console.log('PATCH CONNECTION, new URL<conn.url> = ', conn.url)
+      console.log(
+        'PATCH CONNECTION, new URL<server!.connectionInfo!.url> = ',
+        server!.connectionInfo!.url
+      )
+    }
+
+    return browser
   })
 
   console.log(
